b44d63dd89fcf9d684006efce701f96c0627c98a
lalit4786 <37807685+lalit4786@users.noreply.github.com>
2024-10-07 00:31:52 +0530

    Rename and Delete File

---
+++ FileIO/Rename and Delete File/renameFile.py
@@ -0,0 +1,22 @@
+import os
+
+src_file = 'FileIO/Write CSV File/Resources/countries.csv'
+dest_file = 'FileIO/Write CSV File/Resources/countries2.csv'
+
+try:
+    os.rename(src_file,dest_file)
+except FileNotFoundError as e:
+    print(e)
+except FileExistsError as e:
+    print(e)    
+
+src_file = 'FileIO/Write CSV File/Resources/countries2.csv'
+dest_file = 'FileIO/Write CSV File/Resources/countries.csv'
+
+try:
+    os.rename(src_file,dest_file)
+except FileNotFoundError as e:
+    print(e)
+except FileExistsError as e:
+    print(e)    
+
